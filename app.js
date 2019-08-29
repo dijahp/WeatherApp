1f7a5205f8cacbd868baa643cb3ef4c67ec8cf3b
Sherri Booher <sherribooher74@gmail.com>
2019-08-29 00:29:22 -0400

    Add models, migrations for database

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var bcrypt = require("bcrypt");
+var session = require("express-session");
+var ejs = require("ejs");
+var cookieParser = require("cookie-parser");
+var db = require("./models");
+var SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 var axios = require("axios");
 
@@ -8,28 +13,45 @@ var city = "Atlanta";
 
 var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=8e9a4b4545a680355a95d6c810a6f708`;
 
+var sessionStorage = new SequelizeStore({
+  db: db.sequelize
+});
+
 var app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.use(session({
+  secret: 'appSecret',
+  resave: false,
+  saveUninitialized: true,
+  store: sessionStorage
+}));
+
+sessionStorage.sync();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(express.static(__dirname + "/public"));
 
-app.get("/signup", function(req, res, next) {
+app.get("/signup", function (req, res, next) {
   res.render("signup");
 });
-app.get("/signin", function(req, res, next) {
+app.get("/signin", function (req, res, next) {
   res.render("signin");
 });
 
-app.get("/dashboard", function(req, res, next) {
+app.get("/dashboard", function (req, res, next) {
   res.render("dashboard", {});
 });
 
 var cards = [];
 
-app.post("/signup", function(req, res) {
+app.post("/signup", function (req, res) {
   var email = req.body.email;
   var password = req.body.password;
   var firstName = req.body.firstName;
@@ -45,7 +67,7 @@ app.post("/signup", function(req, res) {
   console.log(location);
 });
 
-app.post("/signin", function(req, res) {
+app.post("/signin", function (req, res) {
   var email = req.body.email;
   var password = req.body.password;
 
@@ -53,6 +75,6 @@ app.post("/signin", function(req, res) {
   console.log(password);
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("listening on port 3000...");
 });
---
+++ migrations/20190829040121-create-user.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password_hash: {
+        type: Sequelize.STRING
+      },
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      },
+      temp_unit: {
+        type: Sequelize.ENUM
+      },
+      city: {
+        type: Sequelize.STRING
+      },
+      state: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('users');
+  }
+};
